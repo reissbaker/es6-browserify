e863d1b7952ecca09fd6246ddbf99b1a5712d588
Matt Baker <matthew.reiss.baker@gmail.com>
2014-01-26 01:12:07 -0800

    Correctly buffer data.

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 var through = require('through'),
     transpiler = require('es6-transpiler');
 
+var buffer = [];
+
 module.exports = function(file) {
-  return through(transform);
+  if(!isES6(file)) return through();
+  return through(buffer, transform);
 };
+
+function isES6(file) {
+  return /\.js$/.test(file);
+};
+
+function buffer(data) {
+  buffer.push(data);
+}
 
 function transform(file) {
   var result = transpiler.run({
-    src: file,
+    src: buffer.join(''),
     disallowUnknownReferences: false
   });
 
   if(result.errors && result.errors.length !== 0) {
-    throw errors;
+    this.emit('error', result.errors);
   }
+
   this.queue(result.src);
+  this.queue(null);
 }
